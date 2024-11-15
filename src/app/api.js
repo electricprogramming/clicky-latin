cef8f9298078ed6d4bd580f025cf61d366503fa9
electricprogramming <yeungj316@gmail.com>
2024-11-15 17:04:45 -0500

    add api.js

---
+++ src/app/api.js
@@ -0,0 +1,61 @@
+const api = {
+  GET: async (id) => {
+    return new Promise((resolve, reject) => {
+      fetch(`https://clickylatin-api.glitch.me?id=${String(id)}`)
+        .then(res => res.json())
+        .then(data => {
+          resolve(data);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
+  },
+  POST: async (data) => {
+    return new Promise((resolve, reject) => {
+      fetch('https://clickylatin-api.glitch.me', {
+        method: 'POST',
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => {
+          resolve(data);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
+  },
+  DELETE: async (id) => {
+    return new Promise((resolve, reject) => {
+      fetch('https://clickylatin-api.glitch.me', {
+        method: 'DELETE',
+        body: String(id)
+      })
+        .then(res => res.json())
+        .then(data => {
+          resolve(data);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
+  },
+  ALL: async () => {
+    return new Promise((resolve, reject) => {
+    fetch('https://clickylatin-api.glitch.me/all')
+      .then(res => res.json())
+      .then(data => {
+        resolve(data)
+      })
+      .catch(err => {
+        console.error(err);
+        reject(err);
+      });
+    });
+  }
+}
+export default api;
