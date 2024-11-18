8bd5792fd1a63fb9424476148ca76c5ce54157de
electricprogramming <yeungj316@gmail.com>
2024-11-17 20:38:53 -0500

    why

--- src/app/create/script.js
+++ src/app/create/script.js
@@ -2,5 +2,6 @@ const createPairButton = document.getElementById('create-pair-btn');
 const saveButton = document.getElementById('save-btn');
 const saveModal = document.getElementById('save-modal');
 saveButton.addEventListener('click', () => {
+  alert ('saving')
   saveModal.style.display = '';
 });
