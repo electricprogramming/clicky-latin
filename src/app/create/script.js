c6ad1243fe47ca7b2679f89179f6be0999c46fe0
electricprogramming <yeungj316@gmail.com>
2024-11-18 12:19:18 -0500

    style the input

--- src/app/create/script.js
+++ src/app/create/script.js
@@ -2,6 +2,5 @@ const createPairButton = document.getElementById('create-pair-btn');
 const saveButton = document.getElementById('save-btn');
 const saveModal = document.getElementById('save-modal');
 saveButton.addEventListener('click', () => {
-  alert ('saving')
-  saveModal.style.display = '';
+  saveModal.style.display = 'flex';
 });
