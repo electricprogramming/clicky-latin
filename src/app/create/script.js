36363566fb0543d0157c4880914b5b233bec512d
electricprogramming <yeungj316@gmail.com>
2024-11-17 20:36:29 -0500

    i am kinda stupid

--- src/app/create/script.js
+++ src/app/create/script.js
@@ -2,5 +2,5 @@ const createPairButton = document.getElementById('create-pair-btn');
 const saveButton = document.getElementById('save-btn');
 const saveModal = document.getElementById('save-modal');
 saveButton.addEventListener('click', () => {
-  saveModal.display = '';
+  saveModal.style.display = '';
 });
