f6c0a72fc57fd0046ec9ac42a8f7e0be468e00cd
electricprogramming <yeungj316@gmail.com>
2024-11-18 16:53:53 -0500

    ok

--- src/app/create/script.js
+++ src/app/create/script.js
@@ -1,12 +1,16 @@
 const createPairButton = document.getElementById('create-pair-btn');
 const saveButton = document.getElementById('save-btn');
 const saveModal = document.getElementById('save-modal');
+const saveNameInput = document.getElementById('save-name-input');
+const saveCancelBtn = document.getElementById('save-cancel-btn');
+const saveConfirmBtn = document.getElementById('save-confirm-btn');
 saveButton.addEventListener('click', () => {
   saveModal.style.display = 'flex';
 });
 saveModal.addEventListener('click', (e) => {
-  console.log(e.target);
-  if (e.target === saveModal) {
-    saveModal.style.display = 'none';
-  }
+  if (e.target === saveModal || e.target === saveCancelBtn) saveModal.style.display = 'none';
+});
+saveConfirmBtn.addEventListener('click', () => {
+  const name = saveNameInput.value;
+  console.log(name);
 });
