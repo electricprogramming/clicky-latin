421c98e498e7741506fce695015086f147e8bedb
electricprogramming <yeungj316@gmail.com>
2024-11-20 14:38:15 -0500

    whoops again

--- src/get-recent-deployment-msg.js
+++ src/get-recent-deployment-msg.js
@@ -2,7 +2,7 @@ export default async function getLastDeploymentMessage() {
   return new Promise((resolve, reject) => {
     fetch('https://clickylatin-api.glitch.me/current-deployment-msg')
       .then(res => res.json())
-      .then(data => console.log(data.message))
-      .catch(err => console.error(err));
+      .then(data => resolve(data.message))
+      .catch(err => reject(err));
   });
 };
