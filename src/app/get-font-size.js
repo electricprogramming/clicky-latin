874ece8366ed1a4779165bd997da73fea827be3c
electricprogramming <yeungj316@gmail.com>
2024-11-20 18:11:42 -0500

    fix font size error

--- src/app/get-font-size.js
+++ src/app/get-font-size.js
@@ -1,6 +1,6 @@
 import getLetterWidth from './get-letter-width.js';
 export default function getFontSize(txt) {
-  const maxSize = 170;
+  const maxSize = 145;
   let size = maxSize;
   while (getLetterWidth(size) * txt.length >= 600) {
     size -= 2.5;
