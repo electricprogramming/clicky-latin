8eba8b911e2e72e6a73835debe7f80fa53433186
electricprogramming <yeungj316@gmail.com>
2024-11-20 12:41:56 -0500

    update

--- src/get-recent-commit-msg.js
+++ src/get-recent-commit-msg.js
@@ -1,13 +1,11 @@
 export default async function getLastCommitMessage() {
   return new Promise((resolve, reject) => {
-    fetch('https://cors-anywhere.herokuapp.com/https://api.github.com/repos/electricprogramming/clicky-latin/commits')
-      .then(res => res.json())
-      .then(data => {
-        resolve(data[0].commit.message);
-      })
-      .catch(err => {
-        console.error(err);
-        reject(err);
+    fetch('https://api.github.com/repos/electricprogramming/clicky-latin/commits')
+      .then(response => response.json())
+      .then(data => data[0].commit.message)
+      .then(commitMsg => resolve(commitMsg))
+      .catch(error => {
+        reject(error);
       });
   });
 };
