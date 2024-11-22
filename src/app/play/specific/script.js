9f2950373dbd0ea9e6c257a9f404bdae0bc280f5
electricprogramming <yeungj316@gmail.com>
2024-11-22 15:59:13 -0500

    yeah

--- src/app/play/specific/script.js
+++ src/app/play/specific/script.js
@@ -1,3 +1,11 @@
 import gameCode from './get-game-code.js';
 import api from '../../api.js';
-api.GET(gameCode).then(res => console.log(res));
+const gameData = await new Promise((resolve, reject) => {
+  api.GET(gameCode)
+    .then(res => resolve(res))
+    .catch(err => {
+      console.error(err);
+      reject(err);
+    });
+});
+document.title = `Play ${gameData.name} on Clicky Latin`;
