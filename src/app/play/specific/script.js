817a84aa47ae0625b3963a50eb76d3dd2eb2011e
electricprogramming <yeungj316@gmail.com>
2024-11-22 15:44:00 -0500

    oh yeah

--- src/app/play/specific/script.js
+++ src/app/play/specific/script.js
@@ -1,10 +1,10 @@
 const homeSVG = document.getElementById('back-home-svg');
 const homeBtn = document.getElementById('back-home-btn');
 homeBtn.addEventListener('mouseenter', () => {
-  homeSVG.style.transform = 'scale(1.1)';
+  homeSVG.style.transform = 'scale(1.0)';
 });
 homeBtn.addEventListener('mouseleave', () => {
-  homeSVG.style.transform = 'scale(1.0)';
+  homeSVG.style.transform = 'scale(0.9)';
 });
 homeBtn.addEventListener('click', () => {
   window.location.href = 'https://clickylatin.vercel.app';
