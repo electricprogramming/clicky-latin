544dc5407053f8cb257636901caf6c3f2ea371d8
electricprogramming <yeungj316@gmail.com>
2024-11-22 20:51:56 -0500

    cursor: pointer

--- src/app/play/specific/script.js
+++ src/app/play/specific/script.js
@@ -2,10 +2,18 @@ import gameCode from './get-game-code.js';
 import api from '../../api.js';
 const gameData = await new Promise((resolve, reject) => {
   api.GET(gameCode)
-    .then(res => resolve(res))
+    .then(gameData => 
+      resolve({
+        gameName: gameData.name,
+        gameItems: gameData.items
+      })
+    )
     .catch(err => {
       console.error(err);
       reject(err);
     });
 });
-document.title = `Clicky Latin - Play ${gameData.name}`;
+document.title = `Clicky Latin - Play ${gameName}`;
+gameItems.forEach(([englishWord, latinWord]) => {
+  
+});
