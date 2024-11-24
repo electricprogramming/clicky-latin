233601cb0a2471b823a8b76ce4e511f3f75b64cb
electricprogramming <yeungj316@gmail.com>
2024-11-24 15:23:12 -0500

    fix recent deployment thingy

--- src/get-recent-deployment-msg.js
+++ src/get-recent-deployment-msg.js
@@ -1,6 +1,6 @@
 export default async function getLastDeploymentMessage() {
   return new Promise((resolve, reject) => {
-    fetch('https://clickylatin-api.glitch.me/current-deployment-msg')
+    fetch(`https://clickylatin-api.glitch.me/current-deployment-msg?timestamp=${Date.now()}`)
       .then(res => res.json())
       .then(data => resolve(data.message))
       .catch(err => reject(err));
