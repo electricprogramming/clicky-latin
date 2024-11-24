6963fcd217a832fac8317b00e89203595421045b
electricprogramming <yeungj316@gmail.com>
2024-11-24 16:42:20 -0500

    whoops

--- src/app/play/specific/script.js
+++ src/app/play/specific/script.js
@@ -16,17 +16,15 @@ const {gameName, gameItems} = await new Promise((resolve, reject) => {
     });
 });
 document.title = `Clicky Latin - Play \`${gameName}\``;
-const allWords = gameItems.map(pair => {
-  return [
-    {
-      language: 'English',
-      value: pair[0]
-    },
-    {
-      language: 'Latin',
-      value: pair[1]
-    }
-  ]
+const allWords = gameItems.map(RETURN, pair => {
+  RETURN({
+    language: 'English',
+    value: pair[0]
+  });
+  RETURN({
+    language: 'Latin',
+    value: pair[1]
+  });
 });
 Array.shuffle(allWords).forEach(({language, value}) => {
   createGameElement(language, value);
