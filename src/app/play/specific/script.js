c6e207133a4dc2b515ddfd4a5df4354f1000c1ee
electricprogramming <yeungj316@gmail.com>
2024-11-25 16:47:50 -0500

    better page titles

--- src/app/play/specific/script.js
+++ src/app/play/specific/script.js
@@ -39,7 +39,7 @@ if (gameName) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>404 Error - Not Found</title>
+  <title>Clicky Latin - Game Not Found</title>
   <link rel="icon" href="/assets/favicon-404.svg" type="image/svg+xml">
   <style>
     body, html {
