8c171d8a1029269efa9b5ea0dde531640de81871
electricprogramming <yeungj316@gmail.com>
2024-11-25 16:32:00 -0500

    fix match id checker

--- src/app/play/specific/are-correspoding-match-ids.js
+++ src/app/play/specific/are-correspoding-match-ids.js
@@ -5,6 +5,10 @@
  * @returns {Boolean}
  */
 export default function areCorrespondingMatchIds(matchId1, matchId2) {
-  const lang1 = matchId1.slice(1), lang2 = matchId2.slice(2);
-  if (![lang1, lang2].includesAll('E', 'L')) return false;
+  const lang1 = matchId1.substring(0,1), lang2 = matchId2.substring(0,1),
+    id1 = matchId1.slice(1), id2 = matchId2.slice(1);
+  return ( 
+    id1 === id2 &&
+    [lang1, lang2].includesAll('E', 'L')
+  );
 }
