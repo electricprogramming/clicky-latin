aed8a2119582ee113944ba67caeac4e0921ce176
electricprogramming <yeungj316@gmail.com>
2024-11-30 12:50:13 -0500

    fix sep

--- src/seperator-elements.js
+++ src/seperator-elements.js
@@ -1,57 +1,59 @@
-class VerticalSep extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({
-        mode: 'open'
-    });
-  }
-  connectedCallback() {
-    this.updateGap();
-  }
-  get gap() {
-    return this.getAttribute('gap') || '10px';
-  }
+(function() {
+  class VerticalSep extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({
+          mode: 'open'
+      });
+    }
+    connectedCallback() {
+      this.updateGap();
+    }
+    get gap() {
+      return this.getAttribute('gap') || '10px';
+    }
 
-  set gap(value) {
-    this.setAttribute('gap', value);
-    this.updateGap();
+    set gap(value) {
+      this.setAttribute('gap', value);
+      this.updateGap();
+    }
+    updateGap() {
+      const gap = this.gap;
+      this.style.display = 'block';
+      this.style.height = `${gap}`;
+      this.style.width = '100%';
+    }
+    // Prevent innerHTML/innerText from rendering.
+    get innerHTML() { return ''; }
+    set innerHTML(value) {}
   }
-  updateGap() {
-    const gap = this.gap;
-    this.style.display = 'block';
-    this.style.height = `${gap}`;
-    this.style.width = '100%';
+  customElements.define('v-sep', VerticalSep);
+  class HorizontalSep extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({
+          mode: 'open'
+      });
+    }
+    connectedCallback() {
+      this.updateGap();
+    }
+    get gap() {
+      return this.getAttribute('gap') || '10px';
+    }
+    set gap(value) {
+      this.setAttribute('gap', value);
+      this.updateGap();
+    }
+    updateGap() {
+      const gap = this.gap;
+      this.style.display = 'block';
+      this.style.width = `${gap}`;
+      this.style.height = '100%';
+    }
+    // Prevent innerHTML/innerText from rendering.
+    get innerHTML() { return ''; }
+    set innerHTML(value) {}
   }
-  // Prevent innerHTML/innerText from rendering.
-  get innerHTML() { return ''; }
-  set innerHTML(value) {}
-}
-customElements.define('v-sep', VerticalSep);
-class HorizontalSep extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({
-        mode: 'open'
-    });
-  }
-  connectedCallback() {
-    this.updateGap();
-  }
-  get gap() {
-    return this.getAttribute('gap') || '10px';
-  }
-  set gap(value) {
-    this.setAttribute('gap', value);
-    this.updateGap();
-  }
-  updateGap() {
-    const gap = this.gap;
-    this.style.display = 'block';
-    this.style.width = `${gap}`;
-    this.style.height = '100%';
-  }
-  // Prevent innerHTML/innerText from rendering.
-  get innerHTML() { return ''; }
-  set innerHTML(value) {}
-}
-customElements.define('h-sep', HorizontalSep);
+  customElements.define('h-sep', HorizontalSep);
+})();
