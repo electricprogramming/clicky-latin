cffa25aee0bd4b7cf425651ef411dacb39cd9643
electricprogramming <yeungj316@gmail.com>
2024-11-30 14:34:24 -0500

    fixify

--- src/app/play/specific/get-game-code.js
+++ src/app/play/specific/get-game-code.js
@@ -1,10 +1,3 @@
-const gameCode = new URL(window.location.href).pathname.replace('/play/', '');
-// Remove gameCode from query if it exists
-const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('gameCode') && false) {
-  urlParams.delete('gameCode');
-  const hash = window.location.hash;
-  const newUrl = window.location.origin + window.location.pathname + (urlParams.toString() ? '?' + urlParams.toString() : '') + hash;
-  window.history.replaceState({}, '', newUrl);
-}
+const url = new URL(window.location.href);
+const gameCode = /\/play\/(.+)/.test(url)? url.pathname.replace('/play/', '') : url.searchParams.get('gameCode');
 export default gameCode;
