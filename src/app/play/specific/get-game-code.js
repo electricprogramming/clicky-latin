322e79b826b791667687dbfdd408c6550008de76
electricprogramming <yeungj316@gmail.com>
2024-11-30 14:35:39 -0500

    prettify

--- src/app/play/specific/get-game-code.js
+++ src/app/play/specific/get-game-code.js
@@ -1,3 +1,5 @@
 const url = new URL(window.location.href);
-const gameCode = /\/play\/(.+)/.test(url)? url.pathname.replace('/play/', '') : url.searchParams.get('gameCode');
+const gameCode = /\/play\/(.+)/.test(url)?
+  url.pathname.replace('/play/', '') :
+  url.searchParams.get('gameCode');
 export default gameCode;
