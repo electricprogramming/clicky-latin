0d429d07e7bc400202b72587c0256dec5d8cd9f6
electricprogramming <yeungj316@gmail.com>
2024-11-30 14:28:42 -0500

    try a redirect

--- src/app/play/specific/get-game-code.js
+++ src/app/play/specific/get-game-code.js
@@ -1,2 +1,10 @@
 const gameCode = new URL(window.location.href).pathname.replace('/play/', '');
+// Remove gameCode from query if it exists
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('gameCode') && false) {
+  urlParams.delete('gameCode');
+  const hash = window.location.hash;
+  const newUrl = window.location.origin + window.location.pathname + (urlParams.toString() ? '?' + urlParams.toString() : '') + hash;
+  window.history.replaceState({}, '', newUrl);
+}
 export default gameCode;
