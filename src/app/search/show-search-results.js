210abd4f939ff820fc2b237667466e85930edd21
electricprogramming <yeungj316@gmail.com>
2024-12-02 17:48:50 -0500

    reverse

--- src/app/search/show-search-results.js
+++ src/app/search/show-search-results.js
@@ -1,6 +1,6 @@
 import showSearchResult from './show-search-result.js';
 export default function showSearchResults(results) {
   results.forEach(({name, id}) => {
-    showSearchResult(name, id);
+    showSearchResult(id, name);
   });
 }
