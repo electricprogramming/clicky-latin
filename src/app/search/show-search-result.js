d93167edcdec7f3f0db4556afe47339600bcf3c7
electricprogramming <yeungj316@gmail.com>
2024-12-02 17:48:20 -0500

    please

--- src/app/search/show-search-result.js
+++ src/app/search/show-search-result.js
@@ -1,10 +1,12 @@
 const container = document.getElementById('results-container');
 export default function showSearchResult(gameId, gameName) {
-  const gameUrl = `https://clickylatin.vercel.app/play/${gameId}`
+  const gameUrl = `https://clickylatin.vercel.app/play/${gameId}`;
+  const elementContainer = document.createElement('div');
   const link = document.createElement('a');
-  link.classList.add('result-element');
+  elementContainer.appendChild(link);
+  elementContainer.classList.add('result-element');
   link.setAttribute('href', gameUrl);
-  container.appendChild(link);
+  container.appendChild(elementContainer);
   const iframe = document.createElement('iframe');
   iframe.src = gameUrl;
   link.appendChild(iframe);
