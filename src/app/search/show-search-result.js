08939b1f0d2f2bfe07529a39d3e2d50690bf1ae7
electricprogramming <yeungj316@gmail.com>
2024-12-02 17:45:09 -0500

    this might work

--- src/app/search/show-search-result.js
+++ src/app/search/show-search-result.js
@@ -1,5 +1,11 @@
 const container = document.getElementById('results-container');
-export default function showSearchResult(id, name) {
+export default function showSearchResult(gameId, gameName) {
+  const gameUrl = `https://clickylatin.vercel.app/play/${gameId}`
   const link = document.createElement('a');
+  link.classList.add('result-element');
+  link.setAttribute('href', gameUrl);
   container.appendChild(link);
+  const iframe = document.createElement('iframe');
+  iframe.src = gameUrl;
+  link.appendChild(iframe);
 }
