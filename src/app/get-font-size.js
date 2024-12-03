3e25a98fc1b461db91f728d5b33bbd59b70c3c4c
electricprogramming <yeungj316@gmail.com>
2024-12-03 12:20:44 -0500

    please work

--- src/app/get-font-size.js
+++ src/app/get-font-size.js
@@ -1,12 +1,14 @@
 import getLetterWidth from './get-letter-width.js';
 /**
+ * 
  * @param {String} txt 
- * @returns {Number}
+ * @param {Number} maxSize?
+ * @param {Number} widthToFit?
+ * @returns 
  */
-export default function getFontSize(txt) {
-  const maxSize = 140;
+export default function getFontSize(txt, maxSize = 140, widthToFit = 575) {
   let size = maxSize;
-  while (getLetterWidth(size) * txt.length >= 575) {
+  while (getLetterWidth(size) * txt.length >= widthToFit) {
     size -= 1;
   }
   return size;
--- src/app/search/show-search-result.js
+++ src/app/search/show-search-result.js
@@ -1,3 +1,4 @@
+import getFontSize from '../get-font-size.js';
 const container = document.getElementById('results-container');
 export default function showSearchResult(gameId, gameName) {
   const gameUrl = `https://clickylatin.vercel.app/play/${gameId}`;
@@ -10,4 +11,21 @@ export default function showSearchResult(gameId, gameName) {
   const iframe = document.createElement('iframe');
   iframe.src = gameUrl;
   link.appendChild(iframe);
+  const svgStr = `
+    <svg width="80vw" height="80vh">
+      <rect width="100%" height="100%" fill="lightblue" stroke="black" stroke-width="5%"/>
+      <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Courier New">abc</text>
+    </svg>
+  `;
+  const parser = new DOMParser();
+  const svgEl = parser.parseFromString(svgStr, 'image/svg+xml').documentElement;
+  svgEl.style.userSelect = 'none';
+  link.appendChild(svgEl);
+  const svgElSize = {
+    width: svgEl.getBoundingClientRect().width,
+    height: svgEl.getBoundingClientRect().height
+  }
+  const text = svgEl.querySelector('text');
+  text.textContent = gameName;
+  text.setAttribute('font-size', getFontSize(gameName, svgElSize.height, svgElSize.width));
 }
