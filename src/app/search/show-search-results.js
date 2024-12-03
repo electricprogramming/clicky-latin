12c567e0c06bc2108ef952ff62ce760dc63d323f
electricprogramming <yeungj316@gmail.com>
2024-12-03 12:32:36 -0500

    remove old results

--- src/app/search/show-search-results.js
+++ src/app/search/show-search-results.js
@@ -1,5 +1,6 @@
 import showSearchResult from './show-search-result.js';
 export default function showSearchResults(results) {
+  document.querySelectorAll('.result-element').forEach(el => el.remove());
   results.forEach(({name, id}) => {
     showSearchResult(id, name);
   });
