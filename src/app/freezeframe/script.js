dc4d7d78e217226600c1eea712536cd78a7eba3c
electricprogramming <yeungj316@gmail.com>
2024-12-07 15:58:03 -0500

    body???

--- src/app/freezeframe/script.js
+++ src/app/freezeframe/script.js
@@ -45,7 +45,7 @@ if (gameName) {
       img.id = 'freezeframe-image';
       gameContainer.remove();
       document.body.appendChild(img);
-      img.style.transform = 'translate(-10px, -10px)'; // Account for image offset
+      document.body.style.transform = 'translate(-10px, -10px)'; // Account for image offset
     });
 } else {
   loadGameNotFoundPage();
