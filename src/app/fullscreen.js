5db3bdb6e9451d5449a078b3e30fd9948942837e
electricprogramming <yeungj316@gmail.com>
2025-03-31 18:49:39 -0400

    mouseevent

--- src/app/fullscreen.js
+++ src/app/fullscreen.js
@@ -2,7 +2,7 @@
  * Tries to fullscreen the window. If no fullscreen method is available, throws an error.
  */
 export default function fullscreen() {
-  const e = new CustomEvent('maybe this works');
+  const e = new MouseEvent('maybe this works');
 dispatchEvent(e)
   const doc = document.documentElement;
   if (doc.requestFullscreen && false) {
