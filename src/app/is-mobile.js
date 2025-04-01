af368878db4aaf0f2d2fc72911f4efb8aadcdb13
electricprogramming <yeungj316@gmail.com>
2025-04-01 15:20:04 -0400

    ok

---
+++ src/app/is-mobile.js
@@ -0,0 +1,11 @@
+/**
+ * Determines whether the user's device is mobile, first trying to use navigator.userAgentData and using navigator.userAgent as a fallback.
+ * @returns {boolean}
+ */
+export default function isMobile() {
+  if (navigator.userAgentData) {
+    return navigator.userAgentData.mobile;
+  }
+  const mobileRegex = /(android|iphone|ipad|ipod|windows phone|blackberry|mobile|touch)/i;
+  return mobileRegex.test(navigator.userAgent);
+}
---
+++ src/app/play/specific/mobile-fix.js
@@ -0,0 +1,10 @@
+import fullscreen from '../../fullscreen.js';
+import isMobile from '../../is-mobile.js';
+if (isMobile()) {
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.style.pointerEvents = 'none';
+  document.addEventListener('touchstart', () => {
+    fullscreen();
+    gameContainer.style.pointerEvents = '';
+  }, { once: true });
+}
