13c7be0a5db2805ac0c0b0a8f68226655bb8d62e
electricprogramming <yeungj316@gmail.com>
2025-04-02 18:11:38 -0400

    oh?

--- src/app/play/specific/create-game-element.js
+++ src/app/play/specific/create-game-element.js
@@ -76,7 +76,7 @@ function _createGameElementDesktop(language, matchId, word) {
       y: parseFloat(window.getComputedStyle(el).top) || 0,
       el
     };
-    const closestElementPos = Array.from(document.querySelectorAll(`.game-element[lang="${isEnglish? 'Latin' : 'English'}']`))
+    const closestElementPos = Array.from(document.querySelectorAll(`.game-element[lang="${isEnglish? 'Latin' : 'English'}"]`))
       .map((otherEl) => {
         return {
           x: parseFloat(window.getComputedStyle(otherEl).left) || 0,
@@ -213,7 +213,7 @@ function _createGameElementMobile(language, matchId, word) {
       y: parseFloat(el.style.top) || 0,
       el
     };
-    const closestElementPos = Array.from(document.querySelectorAll(`.game-element[lang="${isEnglish? 'Latin' : 'English'}']`))
+    const closestElementPos = Array.from(document.querySelectorAll(`.game-element[lang="${isEnglish? 'Latin' : 'English'}"]`))
       .map((otherEl) => {
         return {
           x: parseFloat(otherEl.style.left) || 0,
