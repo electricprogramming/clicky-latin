e3a7cb14e31e1dcd0d331fadc95ecb6e6659574c
electricprogramming <yeungj316@gmail.com>
2025-04-03 13:28:14 -0400

    hm

--- src/app/play/specific/make-el-draggable.js
+++ src/app/play/specific/make-el-draggable.js
@@ -83,6 +83,10 @@ function _makeElDraggableDesktop(el, startDragFunc, endDragFunc) {
   document.addEventListener('touchend', stopDragging);
 }
 
+if (isMobile()) {
+  var isPortrait = window.innerWidth <= window.innerHeight;
+}
+
 /**
  * More compatible version that works for mobile.
  * @param {HTMLElement} el 
@@ -154,27 +158,33 @@ function _makeElDraggableMobile(el, startDragFunc, endDragFunc) {
   };
   document.addEventListener('mouseup', stopDragging);
   document.addEventListener('touchend', stopDragging);
-  window.addEventListener('resize', (e) => {
-    const viewportWidth = window.innerWidth;
-    const viewportHeight = window.innerHeight;
-    const elementWidth = el.getBoundingClientRect().width;
-    const elementHeight = el.getBoundingClientRect().height;
-    let left = parseFloat(el.style.left) || 0;
-    let top = parseFloat(el.style.top) || 0;
-    if (left < 0) {
-      left = 0;
+  window.addEventListener('resize', () => {
+    const oldIsPortrait = isPortrait;
+    isPortrait = window.innerWidth <= window.innerHeight;
+    if (oldIsPortrait !== isPortrait) {
+      [el.style.left, el.style.top] = [el.style.top, el.style.left];
+    } else {
+      const viewportWidth = window.innerWidth;
+      const viewportHeight = window.innerHeight;
+      const elementWidth = el.getBoundingClientRect().width;
+      const elementHeight = el.getBoundingClientRect().height;
+      let left = parseFloat(el.style.left) || 0;
+      let top = parseFloat(el.style.top) || 0;
+      if (left < 0) {
+        left = 0;
+      }
+      if (top < 0) {
+        top = 0;
+      }
+      if (left + elementWidth > viewportWidth) {
+        left = viewportWidth - elementWidth;
+      }
+      if (top + elementHeight > viewportHeight) {
+        top = viewportHeight - elementHeight;
+      }
+      el.style.left = `${left}px`;
+      el.style.top = `${top}px`;
     }
-    if (top < 0) {
-      top = 0;
-    }
-    if (left + elementWidth > viewportWidth) {
-      left = viewportWidth - elementWidth;
-    }
-    if (top + elementHeight > viewportHeight) {
-      top = viewportHeight - elementHeight;
-    }
-    el.style.left = `${left}px`;
-    el.style.top = `${top}px`;
   });
 }
 /**
