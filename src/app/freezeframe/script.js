c2c56037e0972b688104f0c331645eb0b6e154f6
electricprogramming <yeungj316@gmail.com>
2025-04-04 18:50:52 -0400

    conditional

--- src/app/freezeframe/script.js
+++ src/app/freezeframe/script.js
@@ -6,29 +6,27 @@ import loadGameNotFoundPage from '../play/specific/load-game-not-found-page.js';
 import deterministicShuffle from './deterministic-shuffle.js';
 const loadingSpinner = document.getElementById('loading-spinner');
 const { gameName, gameItems } = await new Promise((resolve) => {
-  let flag = false;
-  const channel = new BroadcastChannel('GAME_ITEMS_CHANNEL');
-  channel.onmessage = function(e) {
-    flag = true;
-    resolve(e.data);
-  }
-  channel.postMessage('READY_FOR_GAME_ITEMS');
-  setTimeout(() => {
-    if (!flag) {
-      console.log('Message not recieved, fetching data from API instead.')
-      api.GET(gameCode)
-        .then(gameData => 
-          resolve({
-            gameName: gameData.name,
-            gameItems: gameData.items
-          })
-        )
-        .catch(err => {
-          console.error(err);
-          reject(err);
-        });      
+  if (window.self === window.top) {
+    console.log('Message not recieved, fetching data from API instead.')
+    api.GET(gameCode)
+      .then(gameData => 
+        resolve({
+          gameName: gameData.name,
+          gameItems: gameData.items
+        })
+      )
+      .catch(err => {
+        console.error(err);
+        reject(err);
+      });
+  } else {
+    const channel = new BroadcastChannel('GAME_ITEMS_CHANNEL');
+    channel.onmessage = function(e) {
+      flag = true;
+      resolve(e.data);
     }
-  }, 200);
+    channel.postMessage('READY_FOR_GAME_ITEMS');
+  }
 });
 if (gameName) {
   window.gameName = gameName;
