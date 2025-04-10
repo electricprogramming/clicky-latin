5c9e5b719c0f63ac609183fb396b8b555a9c45ff
electricprogramming <yeungj316@gmail.com>
2025-04-10 16:19:13 -0400

    inherit

--- src/app/special-themes/inherit.js
+++ src/app/special-themes/inherit.js
@@ -1,6 +1,10 @@
-const topDoc = window.top.document.documentElement;
-const thisDoc = document.documentElement;
+if (window.top !== window.self) {
+  const topDoc = window.top.document.documentElement;
+  const thisDoc = document.documentElement;
 
-Array.from(topDoc.style).forEach(v => {
-  thisDoc.style.setProperty(v, topDoc.style.getPropertyValue(v));
-});
+  Array.from(topDoc.style).forEach(v => {
+    thisDoc.style.setProperty(v, topDoc.style.getPropertyValue(v));
+  });
+} else {
+  import ('./apply.js');
+}
