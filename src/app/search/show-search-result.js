e605727dfc759228b702844e7520012d925cd762
electricprogramming <yeungj316@gmail.com>
2025-04-10 15:46:51 -0400

    hmmm

--- src/app/search/show-search-result.js
+++ src/app/search/show-search-result.js
@@ -29,11 +29,9 @@ export default function showSearchResult(gameId, gameName, resultIndex) {
     text.textContent = gameName;
     document.getElementById('results-container').appendChild(container);
 
-    iframe.onload = function() {
-      Array.from(document.documentElement.style).forEach(v => {
-        iframe.contentDocument.documentElement.style.setProperty(v, document.documentElement.style.getPropertyValue(v));
-      });
-    }
+    Array.from(document.documentElement.style).forEach(v => {
+      iframe.contentDocument.documentElement.style.setProperty(v, document.documentElement.style.getPropertyValue(v));
+    });
 
     container.style.order = resultIndex;
     function resizeText() {
