2d0064b12f90cf0c1e0ad24e342e10fe9af38fd7
electricprogramming <yeungj316@gmail.com>
2025-04-10 11:27:39 -0400

    revert

--- src/app/special-days.js
+++ src/app/special-days.js
@@ -1,7 +1,7 @@
 const today = new Date();
 
 export function isDateX() {
-  return today.getMonth() === 3 && today.getDate() === 1;
+  return today.getMonth() === 3 && today.getDate() === 10;
 }
 
 function calcEasterDate(year) {
