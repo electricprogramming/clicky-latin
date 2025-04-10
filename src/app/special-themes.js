6fbccb8c217d04d900aec7a203aa90f81debb619
electricprogramming <yeungj316@gmail.com>
2025-04-10 12:28:47 -0400

    3 and 4

--- src/app/special-themes.js
+++ src/app/special-themes.js
@@ -13,6 +13,8 @@ if (isDateX()) {
   setCssVar('background', '#ff0');
   setCssVar('foreground-1', '#f0f');
   setCssVar('foreground-2', '#0ff');
+  setCssVar('foreground-3', '#f0f');
+  setCssVar('foreground-4', '#0ff');
   setCssVar('main-text', '#000');
   setCssVar('main-ui', '#333');
 }
