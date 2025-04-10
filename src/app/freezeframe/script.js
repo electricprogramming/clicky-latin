0ed42af4311d26547a0741174a4838fad22f5614
electricprogramming <yeungj316@gmail.com>
2025-04-09 20:16:19 -0400

    no log

--- src/app/freezeframe/script.js
+++ src/app/freezeframe/script.js
@@ -8,7 +8,6 @@ const loadingSpinner = document.getElementById('loading-spinner');
 const { gameName, gameItems } = await new Promise((resolve, reject) => {
   const cachedGameData = window.top.cachedGames?.[gameCode];
   if (cachedGameData && typeof cachedGameData === 'object') {
-    console.log(cachedGameData)
     resolve({
       gameName: cachedGameData.name,
       gameItems: cachedGameData.items
