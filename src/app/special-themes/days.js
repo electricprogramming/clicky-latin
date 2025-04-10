ce2fb87e03e736b20e798c7358939d30c20d20bd
electricprogramming <yeungj316@gmail.com>
2025-04-10 17:20:43 -0400

    cycle

--- src/app/special-themes/days.js
+++ src/app/special-themes/days.js
@@ -1,9 +1,16 @@
+let count = sessionStorage.getItem('counter');
+if (count === null) {
+  count = 0;
+} else {
+  count = parseInt(count) + 1;
+}
+sessionStorage.setItem('counter', count);
 const dates = [
   'dec 25', 'apr 20 2025', 'nov 28 2024',
   'oct 31', 'apr 1', 'jul 4',
   'feb 14', 'may 4'
 ]
-const today = new Date(dates[Math.floor(Math.random() * 8)]);
+const today = new Date(dates[count % 8]);
 
 export function isDateX() {
   return today.getMonth() === 3 && today.getDate() === 10;
