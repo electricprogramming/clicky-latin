daac5017fee507ff3238b6ac7abf9c5753b7a606
electricprogramming <yeungj316@gmail.com>
2025-04-10 16:09:41 -0400

    hmmmm

--- src/app/search/show-search-result.js
+++ src/app/search/show-search-result.js
@@ -28,9 +28,11 @@ export default function showSearchResult(gameId, gameName, resultIndex) {
     const text = svgEl.querySelector('text');
     text.textContent = gameName;
     document.getElementById('results-container').appendChild(container);
-    Array.from(document.documentElement.style).forEach(v => {
-      iframe.contentDocument.documentElement.style.setProperty(v, document.documentElement.style.getPropertyValue(v));
-    });
+    setTimeout(() => {
+      Array.from(document.documentElement.style).forEach(v => {
+        iframe.contentDocument.documentElement.style.setProperty(v, document.documentElement.style.getPropertyValue(v));
+      });
+    }, 0);
 
     container.style.order = resultIndex;
     function resizeText() {
