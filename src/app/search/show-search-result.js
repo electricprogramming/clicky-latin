8b0f8262b5b571481cc9d1bf42115769e39676c4
electricprogramming <yeungj316@gmail.com>
2025-04-10 11:57:55 -0400

    domcontentloaded

--- src/app/search/show-search-result.js
+++ src/app/search/show-search-result.js
@@ -29,11 +29,12 @@ export default function showSearchResult(gameId, gameName, resultIndex) {
     text.textContent = gameName;
     document.getElementById('results-container').appendChild(container);
 
-    iframe.onload = function() {
+    iframe.contentDocument.addEventListener('DOMContentLoaded', () => {
         ['background', 'foreground-1', 'foreground-2'].forEach(v => {
         iframe.contentDocument.documentElement.style.setProperty(`--${v}`, document.documentElement.style.getPropertyValue(`--${v}`));
       });
-    }
+    });
+
     container.style.order = resultIndex;
     function resizeText() {
       if (document.contains(container) && getComputedStyle(container).display !== 'none') {
