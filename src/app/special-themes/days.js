0b6d9c5abfbd4f1b48f8d610f389bdf9a977c0c8
electricprogramming <yeungj316@gmail.com>
2025-04-12 08:28:13 -0400

    cycle

--- src/app/special-themes/days.js
+++ src/app/special-themes/days.js
@@ -1,4 +1,12 @@
-const today = new Date();
+if (!localStorage.idx) {
+  localStorage.idx = 0;
+}
+localStorage.idx ++;
+const dates = [
+  'dec 25', 'apr 20 2025', 'nov 28 2024',
+  'oct 31', 'apr 1', 'july 4', 'feb 14', 'may 4'
+]
+const today = new Date(dates[localStorage.idx % 8]);
 
 export function isDateX() {
   return today.getMonth() === 3 && today.getDate() === 10;
