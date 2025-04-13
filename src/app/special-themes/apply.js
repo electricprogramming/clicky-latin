5cb2052859550366db9269e5e762cc83b8cc15ae
electricprogramming <yeungj316@gmail.com>
2025-04-13 12:32:24 -0400

    WHAT ON EARTH

--- src/app/special-themes/apply.js
+++ src/app/special-themes/apply.js
@@ -61,7 +61,7 @@ if (isChristmas()) {
   setCssVar('minor-text', '#ee1');
   setCssVar('bright-text', '#ee1');
   setCssVar('main-ui', '#c0d');
-  setCssVar('ui-accent', '#dd2dd');
+  setCssVar('ui-accent', '#dd2');
 } else if (isAprilFools()) {
   setCssVar('background', '#fff');
   setCssVar('foreground-1', '#0001');
