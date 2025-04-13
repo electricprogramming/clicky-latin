7748fe6320de2424d81ce0201f38857a6d2d4331
electricprogramming <yeungj316@gmail.com>
2025-04-12 20:42:19 -0400

    lowercase

--- src/app/play/specific/get-game-code.js
+++ src/app/play/specific/get-game-code.js
@@ -1,5 +1,5 @@
 const url = new URL(window.location.href);
 const gameCode = /\/play\/(.+)/.test(url)?
   url.pathname.replace('/play/', '') :
-  url.searchParams.get('gameCode');
+  url.searchParams.get('gamecode');
 export default gameCode;
