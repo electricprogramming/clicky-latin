5d7dec3124294a7fb49cf8705021f6075b371c84
electricprogramming <yeungj316@gmail.com>
2025-04-18 11:09:46 -0400

    whoops ok

--- api/delete.js
+++ api/delete.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  fetch('https://clickylatin-api.glitch.me?gamecode=req.query.id', {
+  fetch(`https://clickylatin-api.glitch.me?gamecode=${req.query.gamecode}`, {
     method: 'DELETE',
     headers: {
       Authorization: `Key ${process.env.VALID_DELETE_KEY}`
