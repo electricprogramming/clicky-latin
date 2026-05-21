17cec459ca58f3cddc940093f34274147139e613
electricprogramming <yeungj316@gmail.com>
2026-05-21 12:56:01 -0400

    grammar

--- src/download-warning.js
+++ src/download-warning.js
@@ -35,7 +35,7 @@ if (!localStorage.getItem("downloadWarningAcknowledged")) {
 
   // 4. Create the text content
   const message = document.createElement("p");
-  message.textContent = "IMPORTANT: Clicky Latin will NEVER try to download a file to your device. If it does, Clicky Latin has likely been hacked and the file is likely malware which should be deleted immediately.";
+  message.textContent = "IMPORTANT: Clicky Latin will NEVER try to download a file to your device. If it does, Clicky Latin has likely been hacked, and the file is likely malware which should be deleted immediately.";
   message.style.marginBottom = "5vmin";
   message.style.fontSize = "5.25vmin"
   message.style.setProperty("color", "#fff", "important");
